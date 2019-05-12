5a80acb7e2c62ef7f6f6c8de821ff482ac0482bd
Geo Engel <gengel@hamilton.edu>
2019-05-12 12:32:34 -0400

    back propogation works for increment

--- project4.py
+++ project4.py
@@ -87,24 +87,89 @@ class NeuralNetwork:
 		"""creates a representation of the neural network based on structure"""
 
 		self.network = {}
+		self.structure = structure
 		#create all of the nodes
 		for i in range(len(structure)-1):
-			for  j in range(sum(structure[:i]), sum(structure[:i+1])):
-				self.network[j] = []
+			for j in range(sum(structure[:i]), sum(structure[:i+1])):
+				self.network[j+1] = []
 				for _ in range(structure[i+1]):
-					self.network[j].append(random.uniform(-1, 1))
+					self.network[j+1].append(random.uniform(-1, 1))
+
+		#create dummy node
+		self.network[0] = [random.uniform(-1, 1) for _ in range(sum(structure[1:]))]
 
 		self.outputs = [0] * structure[-1]
+		print(self.network)
 
-	def back_propogation_learning(training):
+
+	def back_propagation_learning(self, training):
 		"""the neural net learns using the back propogation algorithm from the book"""
 
-		
+		t = 0
+		errors = [[1000]]
+
+		while abs(sum([sum(e) for e in errors])) >= .0001 * len(training):
+			errors = []
+			for x, y in training:
+				error = [0] * (sum(self.structure) + 1)
+				values = []
+
+				#propogate input to compute outputs
+				for i in range(self.structure[0] + 1):
+					values.append(x[i])
+
+				#for each layer
+				for i in range(1, len(self.structure)):
+					#for each node in the layer
+					for j in range(self.structure[i]):
+						#for the input nodes
+						inputs = [values[0] * self.network[0][j + sum(self.structure[1:i])]]
+						#for each nonDummy input to the node
+						for n in range(self.structure[i - 1]):
+							#print(n + sum(self.structure[:i-1]) + 1, j, self.network[n + sum(self.structure[:i-1]) + 1][j])
+							inputs.append(values[n + sum(self.structure[:i-1]) + 1] * self.network[n + sum(self.structure[:i-1]) + 1][j])
+						values.append(logistic(sum(inputs)))
+
+				#propogate error backwards
+				#output layer
+				for i in range(self.structure[-1]):
+					#print(-1 - i, y[-1 - i], values[-1 - i], y[-1 - i] - values[-1 - i])
+					error[-1 - i] = (logistic(values[-1 - i]) * (1 - logistic(values[-1 - i]))) * (y[-1 - i] - values[-1 - i])
+
+				#for each layer backwards style 
+				for i in range(len(self.structure) - 2, -1, -1):
+					#for each node in that layer
+					for j in range(self.structure[i]):
+
+						error[j + sum(self.structure[:i]) + 1] = logistic(values[j + sum(self.structure[:i]) + 1]) * \
+						(1 - logistic(values[j + sum(self.structure[:i]) + 1])) * \
+						sum([self.network[j + sum(self.structure[:i]) + 1][k] * error[sum(self.structure[:i + 1]) + k + 1] for k in range(self.structure[i+1])])
+
+				#calculate error of dummy weight
+				error[0] = logistic(values[0]) * (1 - logistic(values[0])) * sum([self.network[0][k] * error[k + self.structure[0] + 1] for k in range(sum(self.structure[1:]))])
+
+				#update the weights
+				for i in range(len(self.structure)-1):
+					for j in range(sum(self.structure[:i]), sum(self.structure[:i+1])):
+						for k in range(self.structure[i+1]):
+							#print(i, j, k, k + sum(self.structure[:i+1]) + 1)
+							self.network[j+1][k] = self.network[j+1][k] + .05 * values[j+1] * error[k + sum(self.structure[:i+1]) + 1]
+
+				#update weights of dummy variable
+				for i in range(len(self.network[0])):
+					self.network[0][i] = self.network[0][i] + .05 * values[0] * error[i + self.structure[0] + 1]
+
+				errors.append(error)
+			#print("network", p, "\n", self.network, '\n')
+
+			print(abs(sum([sum(e) for e in errors])))
+
+			t += 1
+
+
 
 def main():
 	"""main"""
-	nn = NeuralNetwork([3, 6, 3])
-	quit()
 
 	header, data = read_data(sys.argv[1], ",")
 
@@ -119,8 +184,8 @@ def main():
 
 	### I expect the running of your program will work something like this;
 	### this is not mandatory and you could have something else below entirely.
-	# nn = NeuralNetwork([3, 6, 3])
-	# nn.back_propagation_learning(training)
+	nn = NeuralNetwork([3, 6, 3])
+	nn.back_propagation_learning(training)
 
 if __name__ == "__main__":
 	main()
