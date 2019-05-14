d4843dfa4ee5f8c47806f60b3e3eb91fb209940a
Geo Engel <gengel@hamilton.edu>
2019-05-14 18:05:34 -0400

    implemented cross validation

--- project4.py
+++ project4.py
@@ -98,18 +98,17 @@ class NeuralNetwork:
 		#create dummy node
 		self.network[0] = [random.uniform(-1, 1) for _ in range(sum(structure[1:]))]
 
-		self.outputs = [0] * structure[-1]
-		print(self.network)
-
 
 	def back_propagation_learning(self, training):
 		"""the neural net learns using the back propogation algorithm from the book"""
 
-		t = 0
 		errors = [[1000]]
+		t = 0
 
-		while abs(sum([sum(e) for e in errors])) >= .0001 * len(training):
+		#while abs(sum([sum(e) for e in errors])) >= .0001 * len(training):
+		for _ in range(2000):
 			errors = []
+
 			for x, y in training:
 				error = [0] * (sum(self.structure) + 1)
 				values = []
@@ -153,18 +152,83 @@ class NeuralNetwork:
 					for j in range(sum(self.structure[:i]), sum(self.structure[:i+1])):
 						for k in range(self.structure[i+1]):
 							#print(i, j, k, k + sum(self.structure[:i+1]) + 1)
-							self.network[j+1][k] = self.network[j+1][k] + .05 * values[j+1] * error[k + sum(self.structure[:i+1]) + 1]
+							self.network[j+1][k] = self.network[j+1][k] + (1000/(1000 + t)) * values[j+1] * error[k + sum(self.structure[:i+1]) + 1]
 
 				#update weights of dummy variable
 				for i in range(len(self.network[0])):
-					self.network[0][i] = self.network[0][i] + .05 * values[0] * error[i + self.structure[0] + 1]
+					self.network[0][i] = self.network[0][i] + (1000/(1000 + t)) * values[0] * error[i + self.structure[0] + 1]
 
 				errors.append(error)
 			#print("network", p, "\n", self.network, '\n')
 
-			print(abs(sum([sum(e) for e in errors])))
+			# print(abs(sum([sum(e) for e in errors])))
 
 			t += 1
+
+	def estimate(self, x):
+		"""finds an estimate based on the data's input values"""
+
+		values = []
+
+		#propogate input to compute outputs
+		for i in range(self.structure[0] + 1):
+			values.append(x[i])
+
+		#for each layer
+		for i in range(1, len(self.structure)):
+			#for each node in the layer
+			for j in range(self.structure[i]):
+				#for the input nodes
+				inputs = [values[0] * self.network[0][j + sum(self.structure[1:i])]]
+				#for each nonDummy input to the node
+				for n in range(self.structure[i - 1]):
+					#print(n + sum(self.structure[:i-1]) + 1, j, self.network[n + sum(self.structure[:i-1]) + 1][j])
+					inputs.append(values[n + sum(self.structure[:i-1]) + 1] * self.network[n + sum(self.structure[:i-1]) + 1][j])
+				values.append(logistic(sum(inputs)))
+
+		return values[0 - self.structure[-1]:]
+
+def crossValidation(nn, data):
+	"""n-fold cross validation"""
+
+	n = 5
+
+	subsets = [[]] * n
+
+	#split the data into n subsets
+	for i in range(len(data)):
+		subsets[i % n].append(data[i])
+
+	errors = []
+
+	#train and test on each combination of subsets
+	for i in range(n):
+		training = []
+		for subset in subsets:
+			if subset != subsets[i]:
+				training += subset[0]
+
+		#train and test
+		nn.back_propagation_learning(training)
+
+		error = []
+		for j in range(len(subsets[i])):
+			error.append(sum([abs(nn.estimate(subsets[i][j][0])[n] - subsets[i][j][1][n]) for n in range(len(subsets[i][j][1]))]))
+
+		errors.append(sum(error)/(len(error) * len(subsets[0][1])))
+
+	return (sum(errors) / len(errors))
+
+
+
+
+# def testNets(x, y, layerNumMin, layerNumMax, minNodes, maxNodes, step=1):
+# 	"""tests out every neural net within the range of layers each with nodes in the range of nodes"""
+
+# 	structures = []
+# 	for layers in range(layerNumMin, layerNumMax):
+# 		for layer in range(layers):
+# 			for i in range(minNodes, maxNodes, step):
 
 
 
@@ -184,8 +248,23 @@ def main():
 
 	### I expect the running of your program will work something like this;
 	### this is not mandatory and you could have something else below entirely.
-	nn = NeuralNetwork([3, 6, 3])
-	nn.back_propagation_learning(training)
+
+	nn = NeuralNetwork([len(training[0][0]) - 1, 10, len(training[0][1])])
+	print(crossValidation(nn, training))
+	quit()
+
+
+	for j in range(10, 15):
+		nn = NeuralNetwork([3, j, 3])
+		nn.back_propagation_learning(training)
+
+		errors = []
+		for i in range(len(training)):
+			errors.append(sum([abs(nn.estimate(training[i][0])[n] - training[i][1][n]) for n in range(len(training[i][1]))]))
+
+		print(j, sum(errors)/(len(errors) * len(training[0][1])))
+
+
 
 if __name__ == "__main__":
 	main()
